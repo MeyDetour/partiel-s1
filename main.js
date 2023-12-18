e4a871bf2fa7ed9418e507ede88eaf21f4695c74
Mey <146076748+MeyDetour@users.noreply.github.com>
2023-12-18 23:00:56 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function run() {
                 getProducts().then(products => {
                     productList = products
                     renderProducts(productList)
-                    addEvents() //add Events listeners after render elements
+
                 })
             }
         )
@@ -31,7 +31,9 @@ function render(contenu) {
 
 function renderLoginform() {
     let form = `
+   <div class="d-flex flex-column align-items-center">
    
+
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Email address</label>
             <input type="text" name="loginUsername" class="form-control" id="loginUsername" placeholder="Enter username..">
@@ -42,7 +44,7 @@ function renderLoginform() {
         </div>
         <button class="btn btn-primary loginButton">Se connecter</button>
         <a onclick="renderSignupform()" href="#">Sign Up</a>
-
+</div>
     `
     render(form)
     document.querySelector(".loginButton").addEventListener('click', () => {
@@ -53,7 +55,7 @@ function renderLoginform() {
 
 function renderSignupform() {
     let form = `
-  
+     <div class="d-flex flex-column align-items-center">
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Email address</label>
             <input type="text" name="signupUsername" class="form-control" id="signupUsername" placeholder="Create your username..">
@@ -64,7 +66,7 @@ function renderSignupform() {
         </div>
         <a onclick="renderLoginform()" href="#">Login</a>
           <button class="btn btn-primary signupButton">S'enregistrer</button>
-     
+     </div>
     `
     render(form)
     document.querySelector(".signupButton").addEventListener('click', () => {
@@ -86,21 +88,19 @@ function renderProducts() {
         })
 
         contenu = `
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Bonjour ${user.username}</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid w-100 d-flex flex-row justify-content-between">
+                <a class="navbar-brand fs-2" href="#">${user.username}'s shooping cards <i class="bi bi-cart4"></i></a>
+               
+                <div class="" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" onclick="run()" aria-current="page" href="#"><i class="bi bi-arrow-clockwise"></i></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" onclick="clearAll()" href="#">Clear ALl</a>
+                            <a class="nav-link active" aria-current="page" onclick="clearAll()" href="#"><i class="bi bi-x-lg"></i></a>
                         </li>
-                          <li class="nav-item">
+                        <li class="nav-item">
                            <input onchange="changePdp(this)" type="file" name="imagepdp" id="imagepdp" class="inputPDP">
                            <label for="imagepdp">
                                <img src="${getImagePdp()}" alt="photoDeProfil" class="photoDeProfil">
@@ -111,29 +111,66 @@ function renderProducts() {
                 </div>
             </div>
         </nav>
-  
- 
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Name</label>
-            <input type="text" name="productName" class="form-control" id="productName" placeholder="Name of product..">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-         <input type="text" clas="form-control" name="productDesc" class="form-control" id="productDesc" placeholder="Description of product.." >
-        </div>
-            <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Image du produit</label>
-            <input type="file" clas="form-control" name="productImg" class="form-control" id="productImg"  >
-        </div>
-        <button class="btn btn-primary productButton">Create</button>
+  <div class="page">
 
-        <div class="d-flex flex-row flex-wrap">
-        ${productsDesign}
+         <div class="formulaire">
+               <span class="titleForm">Add / Modify item</span>
+                 <div class="d-flex flex-column align-items-center justify-content-around">
+                         <img src="" alt="" class="createImagePreview">
+                 <div class=" d-flex flex-column align-items-start">
+                                         <div class="mb-3">
+                                            <label for="exampleFormControlInput1" class="form-label">Name</label>
+                                            <input type="text" name="productName" class="form-control" id="productName" placeholder="Add item name right here...">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                                         <textarea type="text" clas="form-control" name="productDesc" class="form-control" id="productDesc" placeholder="Add item description right here..." ></textarea>
+                                        </div>
+                                            <div class="mb-3">
+                                            <label for="exampleFormControlTextarea1" class="form-label">Image du produit</label>
+                                            <input type="file" clas="form-control" onchange="preview(this)" name="productImg" class="form-control" id="productImg"  >
+                                        </div>
+                                <span class="text-danger createError"></span>     
+                                <div class="d-flex flex-row">
+                                <button class="btn btn-primary productCreateButton">Create</button>
+                                 <button class="btn btn-primary productEditButton d-none">Edit</button>
+                                 <button class="btn btn-primary productDeleteButton d-none">Delete item</button>
+                    
+                                   </div>
+                </div>
+                         
+                </div>
+
+        </div>
+               
+        <div class="listeProduits d-flex flex-column  ">
+               <form class="d-flex" role="search">
+                    <input class="form-control me-2 ps-5 inputSearch" type="search" placeholder="Type your text here.." aria-label="Search">
+                     <i class="bi bi-search"></i>
+              </form>
+              <div class="d-flex flex-row flex-wrap m-5">
+                ${productsDesign}
+                </div>
+            
+        </div>
+      
 </div>
+        
+        
     `
         render(contenu)
-        document.querySelector(".productButton").addEventListener('click', () => {
-            createProduct(document.querySelector('#productName'), document.querySelector('#productDesc'))
+
+
+        document.querySelector(".productCreateButton").addEventListener('click', () => {
+            let name = document.querySelector('#productName')
+            let desc = document.querySelector('#productDesc')
+
+            if (name.value !== "") {
+                createProduct(name, desc, document.querySelector('#productImg'))
+            } else {
+                document.querySelector('.createError').textContent = "Vous n'avez entrez aucun nom"
+            }
+
         })
     } else {
         render(contenu)
@@ -142,18 +179,37 @@ function renderProducts() {
 
 }
 
+
 function renderProduct(product) {
+let statut = `
+ <button class="btn  productChangeStatut1 " onclick="changeStatut(${product.id})">En attente</button>
+          
+`
+    if(product.status){
+        statut = `  <button class="btn  productChangeStatut =="
+               onClick="changeStatut(${product.id})">Ajouté</button>`
+
+
+    }
+
     let productDesign = `
 <div class="card" style="width: 18rem;">
-      <img src="${getImageProduct(product.picture)}" class="card-img-top" alt="...">
+<div class="imageProduit centered">
+   <img src="${getImageProduct(product.picture)}" class="card-img-top  " alt="${product.name}">
+</div>
+   
       <div class="card-body">
             <h5 class="card-title">${product.name}</h5>
             <p class="card-text">${product.description}</p>
-            <a href="#" class="btn btn-primary productChangeStatut" id="${product.id}">Change statut !</a>
-            <input type="file" onchange="changerImgProduct(this)" class="btn btn-primary productChangeImage" id="${product.id}">Changez l'image !</input>
-          <button class="btn btn-primary" onchange="renderModifyForm(this)" id="${product.id}">Edit</button>
-            <i class="bi bi-trash3" id="${product.id}"></i>
-      </div>
+            <div class=" cardOptions d-flex flex-row justify-content-between">
+                  ${statut}
+                  <div>
+                   <a onclick="renderModifyForm(${product.id})"><i class="bi bi-pencil"></i></a>
+                    <a href="#" onclick="deleteProduct(${product.id})"><i class="bi bi-trash3" id="${product.id}"></i></a> 
+         
+                </div>
+                     </div>
+       </div>
 </div>
     `
 
@@ -161,36 +217,49 @@ function renderProduct(product) {
 
 }
 
-function renderModifyForm(product){
-    id = product.id
-    document.querySelector('#productName')
-    document.querySelector('#productDesc')
-    document.querySelector('#productImg')
+function renderModifyForm(id) {
+
+    let objet = null
+    productList.forEach(prod => {
+        if (prod.id === id) {
+            objet = prod
+        }
+    })
+    console.log(objet)
+    console.log(document.querySelector('#productImg').value)
+
+    let fomName = document.querySelector('#productName')
+    let fomDesc = document.querySelector('#productDesc')
+    let fomImg = document.querySelector('#productImg')
+    let formButton = document.querySelector('.productEditButton')
+    document.querySelector('.productCreateButton').classList.toggle('d-none')
+    document.querySelector('.productDeleteButton').classList.toggle('d-none')
+    formButton.classList.toggle('d-none')
+
+    fomName.value = objet.name
+    fomDesc.value = objet.description
+    view(objet.picture)
+    formButton.innerHTML = "Edit"
+
+
+    formButton.addEventListener('click', () => {
+        console.log(fomName.value)
+        console.log(fomName.value !== "")
+        if (fomName.value !== "") {
+            modify(fomName, fomDesc, fomImg, objet.id)
+        } else {
+            document.querySelector('.createError').textContent = "Vous n'avez entrez aucun nom"
+        }
+
+    })
 }
 
-function addEvents() {
-    document.querySelectorAll('.bi-trash3').forEach(bin => {
-        bin.addEventListener('click', () => {
-            console.log(bin, bin.id)
-            deleteProduct(bin.id)
-
-        })
-    })
-
-    document.querySelectorAll('.productChangeStatut').forEach((product) => {
-
-        product.addEventListener('click', () => {
-            changeStatut(product.id)
-        })
-
-    })
-}
 
 function getImageProduct(picture) {
     if (!picture) {
         return 'image/defaultPorduct.png'
     }
-return picture
+    return picture
 }
 
 function getImagePdp() {
@@ -199,6 +268,31 @@ function getImagePdp() {
     }
     return 'image/defaultImg.png'
 }
+
+function preview(file) {
+    if (file) {
+        let imagePreview = document.querySelector('.createImagePreview')
+        let img = file.files[0]
+        let reader = new FileReader();
+        reader.onloadend = function () {
+            imagePreview.src = reader.result;
+
+        };
+        if (img) {
+            reader.readAsDataURL(img);
+        }
+    }
+
+
+}
+
+function view(file) {
+    if (file) {
+        let imagePreview = document.querySelector('.createImagePreview')
+        imagePreview.src = file;
+    }
+}
+
 
 //=========FETCH========//
 
@@ -270,7 +364,8 @@ async function identify() {
         })
 }
 
-async function createProduct(name, description) {
+async function createProduct(name, description, image) {
+    console.log(image.files)
     if (description === "") {
         description.value = 'Aucune description'
     }
@@ -288,7 +383,9 @@ async function createProduct(name, description) {
     await fetch(`${baseUrl}mylist/new`, param)
         .then(response => response.json())
         .then(data => {
+            changerImgProduct(image, data.id)
             run()
+
         })
 }
 
@@ -362,12 +459,13 @@ async function changePdp(file) {
 
     await fetch(`${baseUrl}profilepicture`, param).then(response => response.json())
         .then(data => {
+
             run()
         })
 
 }
 
-async function changerImgProduct(image) {
+async function changerImgProduct(image, id) {
     console.log(image)
     const formData = new FormData()
     formData.append('itempic', image.files[0])
@@ -382,13 +480,37 @@ async function changerImgProduct(image) {
         }
 
 
-    await fetch(`${baseUrl}mylist/addpicturetoitem/${image.id}`, param).then(response => response.json())
+    await fetch(`${baseUrl}mylist/addpicturetoitem/${id}`, param).then(response => response.json())
         .then(data => {
             console.log(data)
             run()
         })
 }
 
-async function modify(product){
+async function modify(name, description, image, id) {
 
+    if (description === "") {
+        description.value = 'Aucune description'
+    }
+    const param = {
+        method: 'put',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `bearer ${token}`
+        },
+        body: JSON.stringify({
+            name: name.value,
+            description: description.value
+        })
+    }
+    await fetch(`${baseUrl}mylist/edit/${id}`, param)
+        .then(response => response.json())
+        .then(data => {
+            if (image.files[0]) {
+                changerImgProduct(image, data.id)
+            }
+
+            run()
+
+        })
 }
